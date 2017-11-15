747bb535cc5b9f259a1c5f52f8bdf5c51641c613
4010d10 <4010d10@guineo.ece.uprm.edu>
2017-11-15 14:52:21 -0400

    added color and multiple borders

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -7,6 +7,10 @@ public class Lab3Exercises {
 		myFrame.setLocation(900, 400);
 		// center screen
 		myFrame.setSize(200, 200);
+		
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
+        
 		myFrame.setVisible(true);
 	}
 }
