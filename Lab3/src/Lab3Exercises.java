6dc17148eb6048fa20b2958d4ff68a13b46700c9
4010d10 <4010d10@guineo.ece.uprm.edu>
2017-11-15 15:20:54 -0400

    added diagonal lines (commented) and a center oval

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Javier Ortiz Garcia");
-		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.setLocation(900, 400);
 		// center screen
 		myFrame.setSize(200, 200);
--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -25,13 +25,29 @@ public class MyPanelClass extends JPanel {
                         g.setColor(Color.BLACK);
                         g.fillRect(x1, y1, width + 1, height + 1);
                         
-                        //Draw a border
-                        g.setColor(Color.YELLOW);
-                        g.drawRect(x1, y1, width, height);
+                        int centerX = x2/2;
+                        int centerY = y2/2;
+                        //draw oval
+                        g.setColor(Color.LIGHT_GRAY);
+                        g.fillOval(centerX - 55/2, centerY - 55/2, 55, 55);
+                        //g.fillOval(73, 73, 55, 55);
+
                         
-                        int border = 5;
-                      //Draw an inner border
-                        g.setColor(Color.WHITE);
-                        g.drawRect(x1 + border, y1 + border, width - border*2, height - border*2);
+//                        //Draw a border
+//                        g.setColor(Color.YELLOW);
+//                        g.drawRect(x1, y1, width, height);
+//                        
+//                        int border = 5;
+//                      //Draw an inner border
+//                        g.setColor(Color.WHITE);
+//                        g.drawRect(x1 + border, y1 + border, width - border*2, height - border*2);
+//                        
+//                        //top left to bottom right
+//                        g.setColor(Color.RED);
+//                        g.drawLine(x1, y1, x2, y2);
+//                        
+//                        //top right to bottom left
+//                        g.setColor(Color.CYAN);
+//                        g.drawLine(x2, y1, x1, y2);
             }
 }
