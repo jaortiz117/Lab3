f7d7b13bed94a69196cfcbe6421577aeffd01c33
JavierO <javier.ortiz13@upr.edu>
2017-11-16 14:41:12 -0400

    Added polygons

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Insets;
+import java.awt.Polygon;
+
 import javax.swing.JPanel;
  
 public class MyPanelClass extends JPanel {
@@ -25,12 +27,37 @@ public class MyPanelClass extends JPanel {
                         g.setColor(Color.BLACK);
                         g.fillRect(x1, y1, width + 1, height + 1);
                         
-                        int centerX = x2/2;
-                        int centerY = y2/2;
-                        //draw oval
-                        g.setColor(Color.LIGHT_GRAY);
-                        g.fillOval(centerX - 55/2, centerY - 55/2, 55, 55);
-                        //g.fillOval(73, 73, 55, 55);
+//                        Polygon p = new Polygon();
+//                        p.addPoint(x1 + 5, y1 + 25);
+//                        p.addPoint(x1 + 20, y1 + 10);
+//                        p.addPoint(x1 + 35, y1 + 25);
+//                        p.addPoint(x1 + 25, y1 + 25);
+//                        p.addPoint(x1 + 25, y1 + 45);
+//                        p.addPoint(x1 + 15, y1 + 45);
+//                        p.addPoint(x1 + 15, y1 + 25);
+//                        g.setColor(Color.YELLOW);
+//                        g.fillPolygon(p);
+                        
+                        Polygon p2 = new Polygon();
+                        p2.addPoint(x1 + 25, y1 + 73);
+                        p2.addPoint(x1 + 41, y1 + 73);
+                        p2.addPoint(x1 + 47, y1 + 58);
+                        p2.addPoint(x1 + 53, y1 + 73);
+                        p2.addPoint(x1 + 69, y1 + 73);
+                        p2.addPoint(x1 + 56, y1 + 83);
+                        p2.addPoint(x1 + 61, y1 + 98);
+                        p2.addPoint(x1 + 47, y1 + 88);
+                        p2.addPoint(x1 + 34, y1 + 98);
+                        p2.addPoint(x1 + 38, y1 + 83);
+                        g.setColor(Color.WHITE);
+                        g.drawPolygon(p2);
+                        
+//                        //draw oval
+//                        int centerX = x2/2;
+//                        int centerY = y2/2;
+//                        g.setColor(Color.LIGHT_GRAY);
+//                        g.fillOval(centerX - 55/2, centerY - 55/2, 55, 55);
+//                        //g.fillOval(73, 73, 55, 55);
 
                         
 //                        //Draw a border
